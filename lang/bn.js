ed9a766307bbc8b95c27a37098c40ce4e93a14cc
medishop-BD <hasangonipurtraders@gmail.com>
2025-07-07 01:35:22 +0600

    Update bn.js

---
+++ lang/bn.js
@@ -0,0 +1,8 @@
+const lang_bn = {
+  "title": "বাংলাদেশ ডাক্তার ডিরেক্টরি",
+  "search_placeholder": "ডাক্তার, হাসপাতাল, জেলা খুঁজুন...",
+  "degree": "ডিগ্রি",
+  "speciality": "বিভাগ",
+  "hospital": "হাসপাতাল",
+  "district": "জেলা"
+};
