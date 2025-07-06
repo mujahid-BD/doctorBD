c547319458c698d801194686678cd29fc1e2b90e
medishop-BD <hasangonipurtraders@gmail.com>
2025-07-07 01:32:40 +0600

    Update en.js

---
+++ lang/en.js
@@ -0,0 +1,8 @@
+const lang_en = {
+  "title": "Bangladesh Doctor Directory",
+  "search_placeholder": "Search doctor, hospital, district...",
+  "degree": "Degree",
+  "speciality": "Specialty",
+  "hospital": "Hospital",
+  "district": "District"
+};
