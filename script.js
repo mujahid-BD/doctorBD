2a67bb41993155f4f4acfab2c18403c67d2bf3ab
medishop-BD <hasangonipurtraders@gmail.com>
2025-07-07 19:47:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   let doctors = [];
   let filteredDoctors = [];
   let currentPage = 1;
-  const perPage = 6;
+  const perPage = 12;
   let currentLang = 'en';
 
   const dict = () => (currentLang === 'bn' ? lang_bn : lang_en);
